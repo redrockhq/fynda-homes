63cca904793e54e7e88cfc2bb690095e00b4b2bc
chimeziesolomon <chimezie.solomon.cs@gmail.com>
2022-08-13 03:39:58 +0100

    new changes

--- js/pricing.js
+++ js/pricing.js
@@ -1,13 +1,15 @@
 const form = document.getElementById("payForm");
-form.addEventListener("submit", payNow);
+form.addEventListener("submit", payForm);
 
 function payNow(e) {
+    //prevent defult
+    e.preventDefault();
+    //set config
     FlutterwaveCheckout({
         public_key: "FLWPUBK_TEST-18df10b7a4021808db6a5c64e892c643-X",
         tx_ref: "fy_" + Math.floor((Math.random() * 1000000000) + 1),
         amount: document.getElementById("amount").value,
         currency: "NGN",
-
         customer: {
             email: document.getElementById("email").value,
             phonenumber: document.getElementById("phoneNumber").value,
